d635ef4fcc72c13083acf9f090969fab22fae4b7
Petar Petrov <ppetrov@vexelon.net>
2020-03-30 11:02:35 +0200

    Add basis for converting a post to an html page

--- engine/spewer.go
+++ engine/spewer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -20,14 +21,33 @@ import (
 var (
 	layout     = "2006-01-02 15:04:05"
 	extensions = parser.CommonExtensions | parser.AutoHeadingIDs
+	tplHeader  *template.Template
+	tplPost    *template.Template
+	tplFooter  *template.Template
 )
 
+type PostPageData struct {
+	*config.YML
+	Post post
+}
+
 type post struct {
-	filename string
-	date     time.Time
-	title    string
-	subtitle string
-	publish  bool
+	filename  string
+	date      time.Time
+	publish   bool
+	Title     string
+	Subtitle  string
+	Content   template.HTML
+	Permalink string
+	Summary   string
+	ImageURL  string
+	Prev      *post
+}
+
+func loadTemplates() {
+	tplHeader = template.Must(template.ParseFiles(filepath.Join(config.TemplatePath, "header.html")))
+	tplPost = template.Must(template.ParseFiles(filepath.Join(config.TemplatePath, "post.html")))
+	tplFooter = template.Must(template.ParseFiles(filepath.Join(config.TemplatePath, "footer.html")))
 }
 
 func writeArchives(dest *os.File) {
@@ -45,12 +65,12 @@ func writePost(dest *os.File, post *post, data []byte) (err error) {
 		case strings.HasPrefix(line, "---"):
 			parsing = !parsing
 		case strings.HasPrefix(line, "title:"):
-			post.title = strings.TrimSpace(line[7:])
-			if len(post.title) == 0 {
+			post.Title = strings.TrimSpace(line[7:])
+			if len(post.Title) == 0 {
 				return errors.New("post title cannot be empty")
 			}
 		case strings.HasPrefix(line, "subtitle:"):
-			post.subtitle = strings.TrimSpace(line[10:])
+			post.Subtitle = strings.TrimSpace(line[10:])
 		case strings.HasPrefix(line, "date:"):
 			post.date, err = time.Parse(time.RFC3339, line[6:])
 			if err != nil {
@@ -70,16 +90,29 @@ func writePost(dest *os.File, post *post, data []byte) (err error) {
 
 	if post.publish {
 		parser := parser.NewWithExtensions(extensions)
-		md := markdown.ToHTML([]byte(strings.Join(lines, "")), parser, nil)
+		post.Content = template.HTML(markdown.ToHTML([]byte(strings.Join(lines, "")), parser, nil))
 
-		fmt.Println("MD---", len(md))
-		// TODO
-		// f, err := os.Create("/tmp/dat2")
-		// if err != nil {
-		// 	return
-		// }
+		var f *os.File
+		f, err = os.Create("./hdr.html")
+		if err != nil {
+			return
+		}
+		defer f.Close()
 
-		// defer f.Close()
+		pd := &PostPageData{config.YMLConfig, *post}
+
+		// header
+		if err = tplHeader.Execute(f, pd); err != nil {
+			return
+		}
+		// body
+		if err = tplPost.Execute(f, pd); err != nil {
+			return
+		}
+		// footer
+		if err = tplFooter.Execute(f, pd); err != nil {
+			return
+		}
 	} else {
 		fmt.Printf("| Skipped %s (publish = false)\n", post.filename)
 	}
@@ -105,7 +138,7 @@ func writePosts(dest *os.File) ([]*post, error) {
 
 			post := new(post)
 			post.filename = file.Name()
-			post.title = strings.ReplaceAll(post.filename[11:strings.LastIndex(post.filename, ".")], "-", " ")
+			post.Title = strings.ReplaceAll(post.filename[11:strings.LastIndex(post.filename, ".")], "-", " ")
 			post.date, err = time.Parse(layout, post.filename[:10]+" 00:00:00")
 			post.publish = true
 			if err != nil {
@@ -139,6 +172,8 @@ func writePosts(dest *os.File) ([]*post, error) {
 
 // Spew generates website
 func Spew() (err error) {
+	loadTemplates()
+
 	tempDir, err := ioutil.TempFile(config.TempPath, "nenu-gen-")
 	if err != nil {
 		return
